d924ff5bb989cb131fd23d0bc2b14417d3bc177c
Tania Kobal <kbtania1@gmail.com>
2021-10-26 21:28:53 +0300

    fixed duplication of objects at dictionary

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,6 +6,28 @@ namespace Lab3
 {
     class Program
     {
+        //class Number
+        //{
+        //    public string number;
+        //    public Number(string num)
+        //    {
+        //        this.number = num;
+        //    }
+        //    public void CreateNum()
+        //    {
+        //        var factory = new FlyweightFactory();
+
+        //        //var flyweigt = factory.GetFlyweight(num);
+
+        //        for (int i = 0; i < number.Length; i++)
+        //        {
+        //            var a = Convert.ToInt32(System.Char.GetNumericValue(number[i]));
+                    
+        //        }
+                
+        //    }
+        //}
+
         public abstract class Flyweight
         {
             public abstract void Display();
@@ -14,6 +36,7 @@ namespace Lab3
         public class ConcreteFlyweight : Flyweight
         {
             private string key;
+            //private int key;
             private List<List<string>> symbols = new List<List<string>> { 
                 new List<string>{"*****", "*   *", "*   *", "*   *", "*****"},
                 new List<string>{ " **", "  *", "  *", "  *", "*****" },
@@ -39,8 +62,8 @@ namespace Lab3
                     intList.Add(Convert.ToInt32(System.Char.GetNumericValue(key[i])));
                 }
 
-                
-                for (int j = 0; j < intList.Count(); j++) 
+
+                for (int j = 0; j < intList.Count(); j++)
                 {
                     for (int i = 0; i < symbols[0].Count; i++)
                     {
@@ -48,6 +71,7 @@ namespace Lab3
                     }
                     Console.WriteLine("");
                 }
+
             }
         }
 
@@ -56,8 +80,16 @@ namespace Lab3
             private Dictionary<string, Flyweight> flyweights = new Dictionary<string, Flyweight>();
             public Flyweight GetFlyweight(string key)
             {
-                Flyweight flyweight = new ConcreteFlyweight(key);
-                flyweights.Add(key, flyweight);
+                Flyweight flyweight = null;
+                if (flyweights.ContainsKey(key))
+                {
+                    flyweight = flyweights[key];
+                } 
+                else
+                {
+                    flyweight = new ConcreteFlyweight(key);
+                    flyweights.Add(key, flyweight);
+                }
                 return flyweight;
             }
         }
@@ -65,8 +97,11 @@ namespace Lab3
         static void Main(string[] args)
         {
             var factory = new FlyweightFactory();
-            var flyweigt = factory.GetFlyweight("148");
+            
+            var flyweigt = factory.GetFlyweight("337");
+            var flyweigt2 = factory.GetFlyweight("144");
             flyweigt.Display();
+            flyweigt2.Display();
         }
     }
 }
